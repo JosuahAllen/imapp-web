59a3ada56a3bad5fd985fbd81703b0da33103bb0
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2024-03-20 06:03:44 +0800

    init shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      hello <Button className="text-red-600">hi</Button>
+    </div>
+  );
 }
